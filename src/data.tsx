d5b82650bc3df206d64834d250b4044553d4fae2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-03 14:22:05 -0700

    Add sample data and declare productInfo type

---
+++ src/data.tsx
@@ -0,0 +1,37 @@
+import { ProductInfo } from "./types";
+
+export const products: ProductInfo[] = [{
+    id: 1,
+    name: "Philodendron Spiritus Sancti",
+    availability: 3,
+    price: 265,
+    condition: "Fully rooted and actively growing",
+    potSize: 4,
+    growingMedium: "Chunky aroid mix",
+    humidity: "76-84%",
+    image: "https://i0.wp.com/herebutnot.com/wordpress/wp-content/uploads/2021/12/philodendron-spiritus-sancti-Apr-06-2022-scaled.jpg?fit=2560%2C1820&ssl=1"
+},
+{
+    id: 1,
+    name: "Philodendron Spiritus Sancti",
+    availability: 1,
+    price: 250,
+    condition: "Fully rooted and actively growing",
+    potSize: 4,
+    growingMedium: "Chunky aroid mix",
+    humidity: "76-84%",
+    image: "http://cdn.shopify.com/s/files/1/0396/1592/2342/products/philodendron-florida-beauty-plant-36622866809084.jpg?v=1655239860"
+},
+{
+    id: 1,
+    name: "Philodendron Spiritus Sancti",
+    availability: 0,
+    price: 100,
+    condition: "Fully rooted and actively growing",
+    potSize: 3,
+    growingMedium: "Chunky aroid mix",
+    humidity: "76-84%",
+    image: "https://i.etsystatic.com/26466238/r/il/135641/3174926021/il_fullxfull.3174926021_6dbi.jpg"
+},
+]
+
---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export interface ProductInfo {
+    id: number;
+    name: string;
+    availability: number;
+    price: number;
+    condition: string;
+    potSize: number;
+    growingMedium: string;
+    humidity: string;
+    image: string
+}
