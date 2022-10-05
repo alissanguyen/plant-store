71cf0c893ff25f5a37118fe4ffc44d33245d7b88
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-04 17:04:14 -0700

    Add plant types

---
+++ src/plants.ts
@@ -0,0 +1,38 @@
+export const plantTypes = [
+  {
+    id: "aglaonema",
+    name: "Aglaonema"
+  },
+  {
+    id: "alocasia",
+    name: "Alocasia"
+  },
+  {
+    id: "anthurium",
+    name: "Anthurium"
+  },
+  {
+    id: "epipremnum",
+    name: "Epipremnum"
+  },
+  {
+    id: "hoya",
+    name: "Hoya"
+  },
+  {
+    id: "monstera",
+    name: "Monstera"
+  },
+  {
+    id: "philodendron",
+    name: "Philodendron"
+  },
+  {
+    id: "scindapsus",
+    name: "Scindapsus"
+  },
+  {
+    id: "syngonium",
+    name: "Syngonium"
+  }
+];
