a7f7b2d51edf0ee95822ecf768c1c24a8e8d5515
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-04 17:16:44 -0700

    Add routes using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
+import "../src/styles.css"
+import { Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import ShopPage from './pages/ShopPage';
+import Navbar from './components/Navbar/Navbar';
 
 interface Props {
 
 }
 
 const App: React.FC<Props> = () => {
- return (
- <div>Plant Store</div>
- )
+
+    return (
+        <div className="App h-[1400px] lg:h-screen">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/shop" element={<ShopPage />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
