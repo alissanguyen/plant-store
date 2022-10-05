ee6b65062f371b298ec6e7dcfd81c84559d58788
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-04 17:05:08 -0700

    Create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu } from "@mui/material"
+import ShoppingCart from "@mui/icons-material/ShoppingCart"
+import { Router, Route } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+import Logo from "../../assets/logo.svg"
+
+interface Props {
+
+}
+
+const NavLinks = [{
+    id: "#Home",
+    name: "Home",
+    url: "/"
+}, {
+    id: "#Shop",
+    name: "Shop",
+    url: "/shop"
+}, {
+    id: "#AboutUs",
+    name: "About Us",
+    url: "/aboutus"
+}, {
+    id: "#Reviews",
+    name: "Reviews",
+    url: "/reviews"
+}, {
+    id: "#FAQ",
+    name: "FAQ",
+    url: "/faq"
+}]
+
+const Navbar: React.FC<Props> = () => {
+    return (
+        <nav>
+            <div className="AppBar shadow-none bg-transparent flex flex-row justify-between items-center py-5 px-[10%] w-full">
+                <div className="Navbar__Logo text-2xl flex-grow-1 flex items-center">
+                    <img src={Logo} height="25px" className='mr-2' alt="" />
+                    <span className="uppercase">Alissa's Garden</span>
+                </div>
+                <div className="w-1/2 flex flex-row justify-between text-lg z-10">
+                    {NavLinks.map((link) => (<NavLink to={`${link.url}`}>{link.name}</NavLink>))}
+                </div>
+                <div className="mr-10">
+                    <IconButton aria-label="View Cart">
+                        <Badge badgeContent={2} color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
