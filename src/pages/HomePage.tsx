68339666088c7a7e6ea225b8f88636ce619f0c51
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-04 17:15:21 -0700

    Create HomePage layout'

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Header from '../components/Header/Header';
+
+interface Props {
+
+}
+
+const HomePage: React.FC<Props> = ({ }) => {
+    return (
+        <Header />
+    )
+}
+
+export default HomePage
