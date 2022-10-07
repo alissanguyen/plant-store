39c8d5ef4ade03d760d29f45ed43638a25884db4
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:20:03 -0700

    Clean up unused component and add util function to handle data from api

--- src/utils.tsx
+++ src/utils.tsx
@@ -5,6 +5,9 @@ export function handleProductAvailabilityDisplay(count: number) {
     if (count === 1) {
         return "ONLY ONE LEFT"
     }
+    if (count === 2) {
+        return "SELLING FAST"
+    }
     else {
         return null
     }
@@ -13,4 +16,23 @@ export function handleProductAvailabilityDisplay(count: number) {
 export var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  });
+});
+
+
+function dataFormatterSingle(productData: any) {
+    return {
+        id: productData.id,
+        name: productData.name,
+        availability: productData.inventory.available,
+        price: productData.price.raw,
+        image: productData.image.url,
+        status: productData.active,
+        category: productData.categories.slug,
+        description: productData.description,
+        relatedProducts: productData.related_products,
+    }
+}
+
+export function dataFormatterMultiple(productDataArray: any[]) {
+    return productDataArray.map((productData) => dataFormatterSingle(productData))
+}
