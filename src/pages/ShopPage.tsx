0ef58d6fbe63998b8b0a193f42f10bd53226b6b9
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:21:36 -0700

    Add shop page and fetch products data from CommerceJS

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,14 +1,49 @@
 import * as React from 'react';
-import Products from '../components/Products/Products';
+import Categories from '../components/Categories/Categories';
+import Product from '../components/Product/Product';
+import SortByMenu from '../components/SortByMenu/SortByMenu';
+import { commerce } from '../lib/commerse';
+import { ProductInfo } from '../types';
+import { dataFormatterMultiple } from '../utils';
 
 interface Props {
 
 }
 
-const ShopPage: React.FC<Props> = ({ }) => {
+const ShopPage: React.FC<Props> = () => {
+    const [products, setProducts] = React.useState<any>()
+
+    const fetchProducts = () => {
+        commerce.products.list().then((products: any) => {
+            console.log(products.data)
+            const formattedData = dataFormatterMultiple(products.data)
+            setProducts(formattedData)
+        }).catch((error: Error) => {
+            console.log('There was an error fetching the products', error)
+        });
+    }
+    
+    React.useEffect(() => {
+        fetchProducts();
+    }, []);
+
     return (
-        <div> <Products /> </div>
+        <div className='px-[10%] my-12 custom:my-20 flex flex-col'>
+            <div className="flex flex-row items-center">
+                <span className="font-semibold text-4xl sm:text-6xl md:text-7xl mb-10">Shop All Plants</span>
+                <SortByMenu />
+            </div>
+
+            <div className="ProductsLayout grid grid-cols-5 m-auto gap-20">
+                <Categories />
+                <div className='ProductsGrid grid gap-6 col-span-4'>
+                    {products ? (products.map((product: ProductInfo) => <Product product={product} />)) : null}
+                </div>
+
+            </div>
+        </div>
     )
 }
 
 export default ShopPage
+
