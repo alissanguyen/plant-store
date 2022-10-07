09759c41b4171fd3ad2bcb8ca6150541b32943ed
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:19:28 -0700

    Modify homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import Header from '../components/Header/Header';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Thumb1 from "../assets/headerthumb1.png"
+import Thumb2 from "../assets/headerthumb2.png"
+import Thumb3 from "../assets/headerthumb3.png"
+import Thumb4 from "../assets/headerthumb4.png"
+import { Link } from 'react-router-dom';
 
 interface Props {
 
@@ -7,8 +12,48 @@ interface Props {
 
 const HomePage: React.FC<Props> = ({ }) => {
     return (
-        <Header />
+        <div className="flex flex-col custom:grid custom:grid-cols-2 px-[10%] gap-10 xl:gap-20 mt-12 custom:mt-20 mb-10 h-full">
+            <HomePageTextContent />
+            <HomePageThumbs />
+        </div>
     )
 }
 
 export default HomePage
+
+const HomePageTextContent = () => {
+    return (
+        <div className="flex flex-col">
+            <span className='text-7xl font-semibold mb-5 custom:mb-10'>Bring the new breeze to your house.</span>
+            <span className='text-3xl lg:text-5xl font-light mb-5 custom:mb-10'>Our mission is to provide you great plants at great prices.</span>
+            <CatalogButton />
+            <div className='spacer-div h-10 custom:h-16'></div>
+            <span className='text-base lg:text-xl italic leading-relaxed'>We have shipped more than 500+ plants across 47 states in America. We take pride in our services and commitment to the plant community.</span>
+        </div>
+    )
+}
+
+const CatalogButton = () => {
+    return (
+        <Link to="/shop" className='Catalog__Button flex flex-row items-center w-fit text-white px-8 py-3 lg:px-10 lg:py-4 text-lg lg:text-xl rounded-full'>
+            <button className="pr-2">Browse the Catalog</button>
+            <ArrowRightIcon className="text-white h-4 w-4 custom:h-6 custom:w-6" />
+        </Link>
+    )
+}
+
+const HomePageThumbs: React.FC = () => {
+    return (
+        <div className="Header__Thumbs flex flex-col gap-4 ">
+            <div className="grid grid-cols-3 gap-4">
+                <img src={Thumb1} className="col-span-2 object-cover rounded-2xl" alt="" />
+                <img src={Thumb3} className="rounded-2xl bg-cover object-cover h-full" alt="" />
+            </div>
+            <div className="grid grid-cols-3 gap-4 ">
+
+                <img src={Thumb2} className="rounded-2xl object-cover h-full" alt="" />
+                <img src={Thumb4} className="col-span-2 rounded-2xl  object-cover" alt="" />
+            </div>
+        </div>
+    )
+}
