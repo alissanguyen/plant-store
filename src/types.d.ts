dc037c7a234a163b1cb60306d1f4dc7fe56d2fd2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:11:04 -0700

    Add types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,9 +3,35 @@ export interface ProductInfo {
     name: string;
     availability: number;
     price: number;
-    condition: string;
-    potSize: number;
-    growingMedium: string;
-    humidity: string;
-    image: string
+    image: string;
+    status: boolean;
+    category: string;
+    description: string;
+    relatedProducts: any[];
+}
+
+export interface ReviewInfo {
+    id: number;
+    description: string;
+    buyer: string;
+    buyerUsername: string | null;
+    location: string;
+    time: string;
+    from: string
+}
+
+export interface FaqInfo {
+    id: number;
+    question: string;
+    answer: string;
+}
+
+export interface PlantCategory {
+    id: string;
+    name: string
+}
+
+export interface SortOption {
+    option: string;
+    value: string;
 }
