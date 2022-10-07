c900cabbd15583286415b54e347a78b91cc1f773
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:16:18 -0700

    Add Categories component on shop page

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { plantCategories } from '../../categories';
+
+interface Props {
+
+}
+
+const Categories: React.FC<Props> = ({ }) => {
+    return (
+        <div className="uppercase flex flex-col gap-3">
+            <span className="text-3xl font-semibold">CATEGORIES</span>
+            <ol className="flex flex-col text-xl font-light">
+                {plantCategories.map((category) => (
+                    <Link to={`/${category.id}`}><li className="border-b-2 py-3 hover:font-normal hover:text-emerald-800 ease-in duration-75 hover:bg-teal-200 hover:bg-opacity-10">{category.name}</li></Link>
+                ))}
+            </ol>
+
+        </div>
+    )
+}
+
+export default Categories
