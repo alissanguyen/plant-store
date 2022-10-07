5246e97db2980f7500f3243155c5997aeeb3b793
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-06 21:10:28 -0700

    Add custom tailwind config and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,js}", "*.{html,js}"], //like this,
-  purge: ["./src/components/**/*.tsx", "./pages/**/*.tsx"],
+  purge: [
+    "./src/components/**/*.tsx",
+    "./pages/**/*.tsx",
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       screens: {
-        custom: "905px",
+        custom: "905px"
       }
     },
     plugins: []
