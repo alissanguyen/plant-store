140e5b61c468755ff115613a79e71b42a72ff2e6
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-30 20:53:47 -0700

    Add common issues page

---
+++ src/pages/CommonIssuesPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const CommonIssuesPage: React.FC<Props> = ({ }) => {
+ return (
+ <div className='px-[10%] my-12 custom:my-20'>
+    <span className="font-semibold text-4xl sm:text-6xl md:text-7xl sm:mb-5">Common Issues</span>
+
+ </div>
+ )
+}
+
+export default CommonIssuesPage
