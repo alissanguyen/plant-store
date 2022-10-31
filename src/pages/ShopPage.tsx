ed0f7cbba593814b4f62683c2a43f75e06925de5
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-30 20:55:13 -0700

    Add shop page

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import Categories from '../components/Categories/Categories';
 import Product from '../components/Product/Product';
 import SortByMenu from '../components/SortByMenu/SortByMenu';
@@ -7,11 +8,14 @@ import { ProductInfo } from '../types';
 import { dataFormatterMultiple } from '../utils';
 
 interface Props {
-
+    onCartChange: React.Dispatch<React.SetStateAction<number>>;
+    cartItemNumber: number;
 }
 
-const ShopPage: React.FC<Props> = () => {
+const ShopPage: React.FC<Props> = (props) => {
+    // TODO: Add custom type instead of `any`
     const [products, setProducts] = React.useState<any>()
+
 
     const fetchProducts = () => {
         commerce.products.list().then((products: any) => {
@@ -22,10 +26,15 @@ const ShopPage: React.FC<Props> = () => {
             console.log('There was an error fetching the products', error)
         });
     }
-    
+
+    const handleAddToCart = async (productId: string, quantity: number) => {
+        const item = await commerce.cart.add(productId, quantity)
+        props.onCartChange(item.cart)
+    }
     React.useEffect(() => {
         fetchProducts();
     }, []);
+
 
     return (
         <div className='px-[10%] my-12 custom:my-20 flex flex-col'>
@@ -37,7 +46,10 @@ const ShopPage: React.FC<Props> = () => {
             <div className="ProductsLayout grid grid-cols-5 m-auto gap-20">
                 <Categories />
                 <div className='ProductsGrid grid gap-6 col-span-4'>
-                    {products ? (products.map((product: ProductInfo) => <Product product={product} />)) : null}
+                    {products ? (products.map((product: ProductInfo) =>
+
+                        <Product product={product} addToCart={handleAddToCart} handleCartChange={props.onCartChange} cartItemNumber={props.cartItemNumber} />
+                    )) : null}
                 </div>
 
             </div>
