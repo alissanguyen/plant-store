529ad6bc4d429c36c1371681f9a31d0adb32a430
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-30 20:53:16 -0700

    Modify navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu } from "@mui/material"
 import ShoppingCart from "@mui/icons-material/ShoppingCart"
-import { Router, Route } from 'react-router-dom';
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 import Logo from "../../assets/logo.svg"
+import { CartData } from '../../types';
 
 interface Props {
-
+    cart: CartData | null | undefined
 }
 
 const NavLinks = [{
@@ -32,7 +32,9 @@ const NavLinks = [{
     url: "/faq"
 }]
 
-const Navbar: React.FC<Props> = () => {
+const Navbar: React.FC<Props> = (props) => {
+    const location = useLocation();
+
     return (
         <nav>
             <div className="AppBar shadow-none bg-transparent flex flex-row justify-between items-center py-5 px-[10%] w-full">
@@ -41,15 +43,18 @@ const Navbar: React.FC<Props> = () => {
                     <span className="uppercase">Alissa's Garden</span>
                 </div>
                 <div className="w-1/2 flex flex-row justify-between text-lg z-10">
-                    {NavLinks.map((link) => (<NavLink to={`${link.url}`}>{link.name}</NavLink>))}
+                    {NavLinks.map((link) => (<NavLink to={`${link.url}`} className="hover:text-emerald-700 ease-in-out duration-75">{link.name}</NavLink>))}
                 </div>
-                <div className="mr-10">
-                    <IconButton aria-label="View Cart">
-                        <Badge badgeContent={2} color="secondary">
-                            <ShoppingCart />
-                        </Badge>
-                    </IconButton>
-                </div>
+                {!location.pathname.startsWith("/cart") ? (<div className="mr-10">
+                    <Link to={`cart/${props.cart ? props.cart.id : ""}`}>
+                        <IconButton aria-label="View Cart">
+                            <Badge badgeContent={props.cart ? props.cart.totalItems : 0} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </Link>
+                </div>) : null}
+
             </div>
         </nav>
     )
