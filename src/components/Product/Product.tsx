95584bbcdbe888c8d9649b77497899984e228da9
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-30 20:54:01 -0700

    Modify Product component

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { ProductInfo } from '../../types';
 import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton"
-import { formatter, handleProductAvailabilityDisplay } from '../../utils';
+import { currencyFormatter, handleProductAvailabilityDisplay } from '../../utils';
 import AddShoppingCart from "@mui/icons-material/AddShoppingCart"
+import { Link } from 'react-router-dom';
 
 interface Props {
-    product: ProductInfo
+    product: ProductInfo;
+    addToCart: (productId: any, quantity: any) => Promise<void>;
+    handleCartChange: React.Dispatch<React.SetStateAction<number>>
+    cartItemNumber: number;
 }
 
 const Product: React.FC<Props> = (props) => {
@@ -16,17 +20,18 @@ const Product: React.FC<Props> = (props) => {
     const productIsSoldOut = props.product.availability === 0;
     return (
         <div className="ProductCard max-w-full font-light relative">
-            <img className={`CardMedia ${productIsSoldOut ? 'SoldOut' : ''} w-full h-80 bg-center object-cover`} src={props.product.image} title={props.product.name} alt=""></img>
+            <Link to={`/shop/${props.product.id}`}>
+                <img className={`CardMedia ${productIsSoldOut ? 'SoldOut' : ''} w-full h-80 bg-center object-cover`} src={props.product.image} title={props.product.name} alt=""></img>
 
-            {productAvailability ? (<span className="ProductCard__Availability absolute top-3 right-3 bg-gray-500 rounded-full px-3 py-1 text-white font-normal">{productAvailability}</span>) : null}
-            <div>
-                <div className="CardContent justify-between py-5 flex flex-row">
-                    <span className="ProductName pb-2 text-lg xl:text-xl font-semibold">{props.product.name}</span>
-                    <span className="ProductPrice text-base xl:text-lg ml-10 pb-2">{formatter.format(props.product.price)}</span>
-                </div>
-            </div>
+                {productAvailability ? (<span className="ProductCard__Availability absolute top-3 right-3 bg-gray-500 rounded-full px-3 py-1 text-white font-normal">{productAvailability}</span>) : null}
+                <div>
+                    <div className="CardContent justify-between py-5 flex flex-row">
+                        <span className="ProductName pb-2 text-lg xl:text-xl font-semibold">{props.product.name}</span>
+                        <span className="ProductPrice text-base xl:text-lg ml-10 pb-2">{currencyFormatter.format(props.product.price)}</span>
+                    </div>
+                </div></Link>
             <CardActions disableSpacing className="CardActions flex justify-end">
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => { props.addToCart(props.product.id, 1); props.handleCartChange(props.cartItemNumber + 1) }}>
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
