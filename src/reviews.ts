6aeacd0ff3c7c07a2b17d086e43203731184a514
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-10-30 20:52:43 -0700

    Add reviews

--- src/reviews.ts
+++ src/reviews.ts
@@ -2,7 +2,61 @@ import { ReviewInfo } from "./types";
 
 export const reviews: ReviewInfo[] = [
   {
-    id: 13,
+    id: 18,
+    description: "I got the plant in great shape! Thank you so much for the plant and gift!!! What a nice surprise 🤗",
+    buyer: "Aeyanna J.",
+    buyerUsername: null,
+    location: "Boise, ID",
+    time: "10/15/2022",
+    from: "Facebook"
+  },
+  {
+    id: 0,
+    description: "Exceptional quality plants, and packaged perfectly. Couldn't have asked for a better buying experience. Looking forward to future purchases",
+    buyer: "Benny T.",
+    buyerUsername: "@benny",
+    location: "San Diego, CA",
+    time: "10/14/2022",
+    from: "Blossm"
+  },
+  {
+    id: 1,
+    description: "Alissa is great! Highly recommend purchasing from her. She is communicative, helpful, and very kind. Her plants are stunning and competitively priced, and she clearly puts a lot of love into them. She even included a note, gifts, and care instructions with the shipment. Will definitely be coming back to buy again. Thank you!",
+    buyer: "Sophia G.",
+    buyerUsername: null,
+    location: "Prescott, AZ",
+    time: "10/14/2022",
+    from: "Blossm"
+  },
+  {
+    id: 2,
+    description: "Amazing packaging she even included a little note and care instructions! I'd give 10 stars if I could!",
+    buyer: "Lauren D.",
+    buyerUsername: "@delaneyjungle13",
+    location: "Austin, TX",
+    time: "10/14/2022",
+    from: "Blossm"
+  },
+  {
+    id: 3,
+    description: "I got them!! Thank you! Look great. I love the little keychain and stickers. Thanks so much for all of this.",
+    buyer: "Christina P.",
+    buyerUsername: null,
+    location: "Hillsboro, OR",
+    time: "10/13/2022",
+    from: "Blossm"
+  },
+  {
+    id: 17,
+    description: "Amazing all around! The plants were packaged perfectly, I will be buying again!",
+    buyer: "Eryn B.",
+    buyerUsername: "@ebrandecker",
+    location: "Stockton, NJ",
+    time: "10/11/2022",
+    from: "Blossm"
+  },
+  {
+    id: 4,
     description:
       "I received my Spiritus Sancti plant baby and it is gorgeous and healthy! Alissa's love of plants and what she does is amazing. It is reflected in her meticulous and thoughtful packaging. She included little gifts that say so much about her kind nature. Everything about my experience was perfect and I will continue to purchase from Alissa. Wishing her much success. 🥰",
     buyer: "Sophie",
@@ -12,7 +66,7 @@ export const reviews: ReviewInfo[] = [
     from: "Blossm"
   },
   {
-    id: 0,
+    id: 5,
     description:
       "Alissa is the absolute sweetest!!! They gave me a great deal and added on another plant for a great price!!! Very communicative and knowledgable. I will definitely be ordering from Alissa again thank you so much💖 💖 🥰 🌱",
     buyer: "Jordan F.",
@@ -22,7 +76,7 @@ export const reviews: ReviewInfo[] = [
     from: "Blossm"
   },
   {
-    id: 1,
+    id: 6,
     description:
       "10 stars!! Packaging was great! The plant is healthy and beautiful 😍",
     buyer: "Liz S.",
@@ -32,7 +86,7 @@ export const reviews: ReviewInfo[] = [
     from: "Blossm"
   },
   {
-    id: 2,
+    id: 7,
     description:
       "The sweetest seller, highly recommend! I love my extra surprise goodies & the communication she provided 😊",
     buyer: "Makenzie W.",
@@ -42,7 +96,7 @@ export const reviews: ReviewInfo[] = [
     from: "Blossm"
   },
   {
-    id: 3,
+    id: 8,
     description:
       "Girl that packaging was great! Looks like the baby leaf probably won't make it, but the rest of the plant is in great condition! Thank you for the crystal necklace too!",
     buyer: "Victoria L.",
@@ -52,7 +106,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 4,
+    id: 9,
     description: "The mid cut was perfect! They are beautiful! Thank you!",
     buyer: "Tiffany N.",
     buyerUsername: null,
@@ -61,7 +115,7 @@ export const reviews: ReviewInfo[] = [
     from: "Blossm"
   },
   {
-    id: 5,
+    id: 10,
     description: "Alissa is a great seller!",
     buyer: "Taslim K.",
     buyerUsername: null,
@@ -70,7 +124,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 6,
+    id: 11,
     description:
       "I have nothing but wonderful things to say about my experience! She was responsive, helpful with finding the perfect options for me and beyond friendly and sweet. The plant were shipped promptly and arrived in perfect condition. She even included super helpful how-to guide and the most adorable planty stickers 🥰 If you're looking for someone to senf you beautiful plants at the right price, she's your girl. Thank you so much and I can't wait to purchase from you again! 💕",
     buyer: "Hannah D.",
@@ -80,7 +134,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 7,
+    id: 12,
     description: "The plant is so pretty. Thank you so much!",
     buyer: "Chris P.",
     buyerUsername: null,
@@ -89,7 +143,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 8,
+    id: 13,
     description:
       "I received the plant yesterday. 1 leaf broke off but otherwise, looks good. I love how pretty she is!! Thank you!!",
     buyer: "Linda L.",
@@ -99,7 +153,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 9,
+    id: 14,
     description:
       "I just received the PSS and it's beautiful!! So so happy with my order, packaging, and gifts!! Thank you so so much!",
     buyer: "Christian L.",
@@ -109,7 +163,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 10,
+    id: 15,
     description:
       "My forgetii made it! Thank you so much for the careful packaging and stickers! I'm so excited!",
     buyer: "Leah S.",
@@ -120,7 +174,7 @@ export const reviews: ReviewInfo[] = [
   },
 
   {
-    id: 11,
+    id: 16,
     description:
       "Girl that packaging was great! Looks like the baby leaf probably won't make it, but the rest of the plant is in great condition! Thank you for the crystal necklace too!",
     buyer: "Megan D.",
@@ -130,7 +184,7 @@ export const reviews: ReviewInfo[] = [
     from: "Facebook"
   },
   {
-    id: 12,
+    id: 17,
     description:
       "She shipped so fast and she looks AMAZING 😍 thank you so so much!!",
     buyer: "Hailee P.",
